ab411ec25c79a6b645987131da391c7eb65f1c07
itay.ariely <itay.ariely@gmail.com>
2016-04-27 21:31:58 +0300

    a

--- app/controllers/circuls_controller.rb
+++ app/controllers/circuls_controller.rb
@@ -4,7 +4,8 @@ class CirculsController < ApplicationController
   # GET /circuls
   # GET /circuls.json
   def index
-    @circuls = Circul.where(father_circul_id: 0)
+    # @circuls = Circul.where(father_circul_id: 0)
+    @circuls = Circul.where("father_circul_id IN ('0','') OR father_circul_id IS NULL")
   end
 
   # GET /circuls/1
