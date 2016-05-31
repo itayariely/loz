2765fd6dc1879f619a5265527514bf2adf0b9b62
itay.ariely <itay.ariely@gmail.com>
2016-05-31 09:16:44 +0300

    d

--- db/migrate/20160409183424_create_friends.rb
+++ db/migrate/20160409183424_create_friends.rb
@@ -1,13 +1,13 @@
-class CreateFriends < ActiveRecord::Migration
-  def change
-    create_table :friends do |t|
-      t.string :name
-      t.string :lname
-      t.string :phone
-      t.string :email
-      t.date :birth_date
-
-      t.timestamps null: false
-    end
-  end
-end
+# class CreateFriends < ActiveRecord::Migration
+#   def change
+#     create_table :friends do |t|
+#       t.string :name
+#       t.string :lname
+#       t.string :phone
+#       t.string :email
+#       t.date :birth_date
+#
+#       t.timestamps null: false
+#     end
+#   end
+# end
